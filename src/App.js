30563736947bb21f241c3129dca893a546369549
Sowrerajan <sowrerajanofficial@gmail.com>
2022-04-22 20:18:01 +0530

    menubar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Add,
+  Chat,
+  FavoriteRounded,
+  Notifications,
+  Person,
+  QuestionMark,
+} from "@mui/icons-material";
+import { useEffect } from "react";
+import "./App.css";
+import MenuContainer from "./Components/MenuContainer";
 
 function App() {
+  useEffect(() => {
+    const allIcon = document.querySelectorAll(".iconContainer");
+    function setMenuActice() {
+      allIcon.forEach((icon) => icon.classList.remove("active"));
+      this.classList.add("active");
+    }
+    allIcon.forEach((icon) => {
+      icon.addEventListener("click", setMenuActice);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menuContainer">
+        <img src="/Images/pinterestLogo.png" alt="logo" className="logo" />
+        <div className="subMenu">
+          <div>
+            <MenuContainer icon={<Person />} />
+            <MenuContainer icon={<Notifications />} />
+            <MenuContainer icon={<Chat />} />
+          </div>
+          <div>
+            <MenuContainer icon={<FavoriteRounded />} />
+          </div>
+          <div>
+            <MenuContainer icon={<QuestionMark />} />
+            <MenuContainer icon={<Add />} />
+          </div>
+        </div>
+      </div>
+      <main></main>
     </div>
   );
 }
